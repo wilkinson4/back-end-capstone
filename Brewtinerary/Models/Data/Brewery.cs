1b976728fea393198b3585a8ed9ae68276202e2a
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-12-09 15:28:31 -0600

    Added a Brewery Model

---
+++ Brewtinerary/Models/Data/Brewery.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Capstone.Models.Data
+{
+    public class Brewery
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string Zip { get; set; }
+
+        [NotMapped]
+        public string FullAddress => $"{Street}, {City}, {State}, {Zip}";
+
+        [Required]
+        public string Longitude { get; set; }
+
+        [Required]
+        public string Latitude { get; set; }
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [Url]
+        public string WebsiteURL { get; set; }
+    }
+}
