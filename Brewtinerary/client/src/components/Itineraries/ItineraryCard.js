0a12ce2b46f6f57ce22268f1e90ad3a37202a630
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-12-13 14:51:53 -0600

    Finished cleaning up itineraries

--- Brewtinerary/client/src/components/Itineraries/ItineraryCard.js
+++ Brewtinerary/client/src/components/Itineraries/ItineraryCard.js
@@ -7,6 +7,7 @@ function ItineraryCard(props) {
             <h2>{props.itinerary.name}</h2>
             <p>{props.itinerary.dateOfEvent}</p>
             <p>{`${props.itinerary.city}, ${props.itinerary.state}`}</p>
+            <button>View Details</button>
         </div>
     )
 }
